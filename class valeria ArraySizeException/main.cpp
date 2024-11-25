a95a0067c7b46b53f1a812c9a1f5b8b98c8020ce
valeriabukova <valeriasergeevna688@gmail.com>
2024-11-25 22:22:58 +0200

    Initial Commit

---
+++ class valeria ArraySizeException/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  class valeria ArraySizeException
+//
+//  Created by Valeria  Bukova on 25.11.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
